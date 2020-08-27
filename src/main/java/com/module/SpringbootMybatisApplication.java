c035a0265d16802da1464b5eb8b15567eb876281
Zhouxin <245357619@qq.com>
2020-08-27 23:29:27 +0800

    添加部署配置

--- src/main/java/com/module/SpringbootMybatisApplication.java
+++ src/main/java/com/module/SpringbootMybatisApplication.java
@@ -4,6 +4,8 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -12,8 +14,16 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan(basePackages= "com.module.config")
 @ComponentScan(basePackages= "com.module")
 public class SpringbootMybatisApplication {
+//注解掉的部分用于打包部署在服务器tomcat时使用
+//public class SpringbootMybatisApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootMybatisApplication.class, args);
 	}
+
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+//		return builder.sources(SpringbootMybatisApplication.class);
+//	}
 }
+
