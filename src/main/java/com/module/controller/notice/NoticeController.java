c1afde5c088ec00e4a0d23ba12695d882abb8af1
hbt <775259966@qq.com>
2020-08-27 16:08:21 +0800

    设备,公告 路径,错误码修改

--- src/main/java/com/module/controller/notice/NoticeController.java
+++ src/main/java/com/module/controller/notice/NoticeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/notice/")
+@RequestMapping("/notice")
 public class NoticeController implements NoticeControllerApi {
 
     @Autowired
