29503449d6385d60b4a6c1316d20ad9276f3cff5
Zhouxin <245357619@qq.com>
2020-09-01 15:11:25 +0800

    新增入库重量预警

---
+++ src/main/java/com/module/request/warnInWeight/WarnInWeightRequest.java
@@ -0,0 +1,20 @@
+package com.module.request.warnInWeight;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+/**
+ * Created by Zhouxin on 2020/8/24;
+ * 将接口接收到的请求数据封装在这里
+ */
+@Data
+public class WarnInWeightRequest {
+
+    @ApiModelProperty("入库重量预警id")
+    private String warnInWeightId;
+
+    @ApiModelProperty("处理状态")
+    private String status;
+
+
+}
