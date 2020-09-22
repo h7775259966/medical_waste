51463508f7b056943eead66cc9386021e1928888
hbt <775259966@qq.com>
2020-09-22 20:26:08 +0800

    新增收集历史(仅查询)

---
+++ src/main/java/com/module/dao/trash/trashCollectHistory/TrashCollectHistoryDao.java
@@ -0,0 +1,19 @@
+package com.module.dao.trash.trashCollectHistory;
+
+import com.common.CrudDao.CrudDao;
+import com.module.entity.trash.trashCollectHistory.TrashCollectHistory;
+import com.module.entity.trash.trashPut.TrashPut;
+import com.module.request.trash.trashCollectHistory.TrashCollectHistoryRequest;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+/**
+ * Created by huangbotao on 2020/8/31;
+ */
+@Mapper
+public interface TrashCollectHistoryDao extends CrudDao<TrashPut> {
+
+   public List<TrashCollectHistory> findHistory(TrashCollectHistoryRequest trashCollectHistoryRequest);
+
+}
