22cab65d829d75a546df7fe877ddd6d5ca22924d
Zhouxin <245357619@qq.com>
2020-09-22 21:44:53 +0800

    新增jwt加密和用户登录接口，以及区县级模块

---
+++ src/main/java/com/module/dao/hospital/zone/ZoneDao.java
@@ -0,0 +1,26 @@
+package com.module.dao.hospital.zone;
+
+import com.common.CrudDao.CrudDao;
+import com.module.entity.hospital.zone.Zone;
+import com.module.request.hospital.zone.ZoneRequest;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+
+/**
+ * 区县级单位DAO接口
+ * @author zx
+ * @version 2020-08-24
+ */
+@Mapper
+public interface ZoneDao extends CrudDao<Zone>{
+
+    /**
+     * 通过查询条件查询所有数据
+     * @param zoneRequest
+     * @return
+     */
+    public List<Zone> findListByRequest(ZoneRequest zoneRequest);
+
+}
