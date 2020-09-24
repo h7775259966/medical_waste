b5751bcf5fda152e5c22c56bd16d405f9007f567
Zhouxin <245357619@qq.com>
2020-09-24 10:10:15 +0800

    优化结构

--- src/main/java/com/common/Response/Response.java
+++
@@ -1,9 +0,0 @@
-package com.common.Response;
-
-/**
- * Created by Zhouxin on 2020/8/24;
- */
-public interface Response {
-    public static final boolean SUCCESS = true;
-    public static final int SUCCESS_CODE = 10000;
-}
